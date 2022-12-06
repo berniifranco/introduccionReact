9a783d9a11e9953ec2815af9002a283ccfc97918
Bernardo Franco <berniifranco@gmail.com>
2022-12-06 02:38:43 -0300

    arreglado nombre del componente Saludo

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import Saludo from './components/Saludos';
+import Saludo from './components/Saludo';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
